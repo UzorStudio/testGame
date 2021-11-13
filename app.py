b4f8e6a3e8e0ffeb5b5b113757aa0fc24cc6d191
UzorStudio <hatkelt@mail.ru>
2021-11-13 05:08:38 +0300

    comm 1

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, url_for, request, redirect, session
 import base
 from datetime import datetime
 
+#db = base.Offers("mongodb+srv://admin:yapidor@cluster0.dautj.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-5utcvk-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true")
 db = base.Offers("localhost")
 app = Flask(__name__)
 app.secret_key = "efwegfewrgeqrgergfhjhlyujmfgvf234234"
+print(db.get_offer_by_date(yaar=2021,month=11,day=12))
 
 @app.route("/")
 def index():
@@ -16,17 +18,22 @@ def logout():
         db.setUserStatus(session["user"], "logout")
         session.pop("user", None)
         return redirect("/login")
-    except KeyError:
+    except KeyError as e:
+        print(e)
         return redirect("/login")
 
 @app.route("/login",methods=["POST","GET"])
 def login():
     if request.method == "POST":
-        print("sadasd")
+        print("1")
+
         name = request.form["name"]
         password = request.form["password"]
+
         print(db.getUserByNic(name))
+
         if session == {} and db.getUserByNic(name) is None:
+            print("2")
             print(session)
             session["user"] = name
             db.regUser(name, password, "")
@@ -70,14 +77,16 @@ def createOffer():
         address = request.form["address"]
         offname = request.form["offname"]
         byname = request.form["byname"]
+        performer = request.form["performer"]
 
-        db.createOffer(nameBy=byname, phone=phone, count=count, address=address, offname=offname)
+
+        db.createOffer(nameBy=byname, phone=phone, count=count, address=address, offname=offname,performer=performer)
         return redirect("/all_offer")
     else:
         try:
             usr = db.getUserByNic(session["user"])["type"]
             if usr == "admin":
-                return render_template("create_offer.html")
+                return render_template("create_offer.html",prf = db.getPerformer())
             else:
                 return "Получите нужный статус у администратора"
         except:
@@ -102,7 +111,7 @@ def updateOffer(id,status):
 def all_offer_for_performer():
 
     usr = db.getUserByNic(session["user"])["type"]
-    off = db.all_offrers()
+    off = db.all_offrers_by_performer(session["user"])
     print("ok")
     if usr == "performer":
         return render_template("all_offer_for_performer.html", off=off)
--- base.py
+++ base.py
@@ -23,23 +23,40 @@ class Offers():
 
         return User.find_one({"name":name})
 
+    def getPerformer(self):
+        db = self.classter["Shop"]
+        User = db["User"]
+
+        cursor = User.find({})
+        usr = []
+        for document in cursor:
+            if usr["type"] == "performer":
+                usr.append(document)
+        return usr[::-1]
+
     def setUserStatus(self,name,status):
         db = self.classter["Shop"]
         User = db["User"]
         User.update_one({"name":name},{"$set":{"status":status}})
 
 
-    def createOffer(self,nameBy,count,phone,address,offname):
+    def createOffer(self,nameBy,count,phone,address,offname,performer):
         db = self.classter["Shop"]
         Offer = db["offer"]
+        data = datetime.datetime.utcnow().date().timetuple()
+        time = {"yar":data[0],
+                "month":data[1],
+                "day":data[2]}
+
         post={"nameBy":nameBy,
               "offname":offname,
               "count":count,
               "address":address,
               "phone":phone,
               "status":"none",
-              "time":datetime.datetime.now()}
-        print(datetime.datetime.now())
+              "time":time,
+              "performer":performer}
+        print(datetime.datetime.utcnow().date())
         Offer.insert_one(post)
 
     def setOfferStatus(self,id,stat):
@@ -53,6 +70,42 @@ class Offers():
         Offer = db["offer"]
 
         cursor = Offer.find({})
+        of = []
+        for document in cursor:
+            of.append(document)
+        return of[::-1]
+
+    def all_offrers_by_performer(self,performer):
+        db = self.classter["Shop"]
+        Offer = db["offer"]
+
+        cursor = Offer.find({})
+        of = []
+        for document in cursor:
+            if document["performer"] == performer:
+                of.append(document)
+        return of[::-1]
+
+    def get_offer_by_date(self,yaar,month,day):
+        db = self.classter["Shop"]
+        Offer = db["offer"]
+
+        time = {"yar": yaar,
+                "month": month,
+                "day": day}
+
+        return Offer.find_one({"time": time})
+
+    def get_offer_NOW(self,):
+        db = self.classter["Shop"]
+        Offer = db["offer"]
+
+        data = datetime.datetime.utcnow().date().timetuple()
+        time = {"yar": data[0],
+                "month": data[1],
+                "day": data[2]}
+
+        cursor = Offer.find({"time": time})
         of = []
         for document in cursor:
             of.append(document)
